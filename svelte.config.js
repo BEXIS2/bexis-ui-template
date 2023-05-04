3a46ee56951b104faa80ba6c54d0ab01b36c1e15
David Schöne <david.blaa@googlemail.com>
2023-05-04 17:05:43 +0200

    change import preprocess from 'svelte-preprocess';

--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,15 @@
 import adapter from '@sveltejs/adapter-static';
 
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import preprocess from 'svelte-preprocess';
 import { build } from 'vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: preprocess(), //vitePreprocess(),
+
 
 	kit: {
 		adapter: adapter({
