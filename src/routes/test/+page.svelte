c6c56856ed4a23a39c066e4333d5cfc573e5b717
David Schöne <david.blaa@googlemail.com>
2023-05-05 10:16:04 +0200

    add test api example

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -4,14 +4,28 @@ import { SlideToggle } from '@skeletonlabs/skeleton';
 
 import type {FileInfo} from '@bexis2/bexis2-core-ui'
 import {Api} from '@bexis2/bexis2-core-ui'
+import { setApiConfig }  from '@bexis2/bexis2-core-ui'
+
+import { onMount } from 'svelte'; 
+
+onMount(async () => {
+   console.log("start entity template");
+   setApiConfig("https://localhost:44345","davidschoene","123456");
 
 
+   try {
+    const response = await Api.get('/dcm/entitytemplates/Load');
+    console.log("data",response.data);
+  } catch (error) {
+    console.error(error);
+  }
+})
 
 $:toggle = false;
 
 </script>
-<h1>Test with layout</h1>
-
+<h1>Test Api</h1>
+<b>check console for more information</b>
 
 <SlideToggle name="david" bind:checked={toggle}></SlideToggle>
 <br/>
